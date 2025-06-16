fada3b1cbe74802fbe0c0338c1c7ecf0fbcc7987
kkarhan <kevin@karhan.guru>
2025-06-16 10:07:48 +0200

    Scripts were expanded upon their TODOs

--- scripts/bash/backup.config.sh
+++ scripts/bash/backup.config.sh
@@ -1,1 +1,8 @@
 #! /sr/bin/env bash
+
+#	Backup existing config from a host with rsync
+##	rsync -av ...
+
+#	Create a commit to the `backup` branch with Commit message `Backup of $HOST @ $UNIXTIME - automated script`
+##	git ...
+
--- scripts/bash/deploy.config.sh
+++ scripts/bash/deploy.config.sh
@@ -1,1 +1,10 @@
 #! /sr/bin/env bash
+
+#	Package config for a host with rsync
+##	rsync -av ...
+
+#	Create a commit to the `deploy` branch with Commit message `Deployment of $HOST @ $UNIXTIME - automated script`
+##	git ...
+
+#	Unpack the config and install it.
+##	tar -xzf ...
